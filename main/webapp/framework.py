3e45658374c05b48ec4af4a2561f3e2eb50be0c5
NAOKI NAGAKURA <nagakura@nagakura-3.local>
2019-05-27 23:33:57 +0900

    add framework & template index.html

--- main/webapp/framework.py
+++ main/webapp/framework.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
  
 @app.route('/')
 def hello():
-    hello = "Hello world"
-    return hello
- 
+    html = render_template('index.html')
+    return html
+
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port=8080)

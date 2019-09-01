55446d8ae1b750b347f447f073bd2b033bcc1b20
EC2 Default User <ec2-user@ip-172-31-20-105.us-east-2.compute.internal>
2019-09-01 03:39:19 +0000

    debug convert_csv_to_json.py

--- main/webapp/json_handler/convert_csv_to_json.py
+++ main/webapp/json_handler/convert_csv_to_json.py
@@ -3,14 +3,19 @@ import os
 import csv
 import glob
 
-def convert_csv_to_json(input_path, output_path):
+def convert_csv_to_json(input_path_list, output_path):
 
     json_list = []
 
     # CSV ファイルの読み込み
-    with open(input_path, 'r') as f:
-        for row in csv.DictReader(f):
-            json_list.append(row)
+    for bus_route_index, input_path in enumerate(input_path_list):
+        print(input_path)
+        with open(input_path, 'r') as f:
+            for timestamp_index, row in enumerate(csv.DictReader(f)):
+                row['busroute_index'] = bus_route_index
+                row['timestamp_index'] = timestamp_index
+                print(row)
+                json_list.append(row)
 
     # JSON ファイルへの書き込み
     with open(output_path, 'a') as f:
@@ -23,6 +28,5 @@ if __name__ == "__main__":
         os.remove(output_path)
 
     ## csvファイルに対するループ
-    for csv_file in glob.glob("%s/*.csv" % input_dir):
-        print(csv_file)
-        convert_csv_to_json(csv_file, output_path)
+    csv_file_list = glob.glob("%s/*.csv" % input_dir)
+    convert_csv_to_json(csv_file_list, output_path)
